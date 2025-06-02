62b8d85e65d1141abd56768387fc27b8de7ee8f1
Manoel Albino Neto <Manoel.Aneto@fornecedores.sicoob.com.br>
2025-06-02 18:21:52 -0300

    configuracao para variaveis de ambiente .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
-import * as process from 'process';
-
 export const environment = {
   url: "https://franquia-agencia-pagamento-0f658190d6af.herokuapp.com",
   production: true,
-  token: `${process.env['TOKEN']}`   
+  token: process.env['TOKEN'] || 'empty'
 };
