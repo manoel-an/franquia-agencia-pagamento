d200bd6981ad5fe43cb6e528278aa5314f7a8077
Manoel Albino Neto <Manoel.Aneto@fornecedores.sicoob.com.br>
2025-06-02 21:23:39 -0300

    retirada log token

--- src/app/thankYou/thankYou.component.ts
+++ src/app/thankYou/thankYou.component.ts
@@ -47,8 +47,6 @@ export class ThankYouComponent implements OnInit {
 
     try {
 
-      console.log(`TOKEN = ${token}`);
-
       const file = await this.githubService.getFile(owner, repo, path, token).toPromise();
 
       sha = file.sha;
